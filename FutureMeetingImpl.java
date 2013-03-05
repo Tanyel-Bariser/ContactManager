34db02bd93c0e36bb20722fac0156b297a54fa90
Tanyel Bariser <tanyel19@hotmail>
2013-03-05 19:25:22 +0000

    Added second constructor @param Meeting meeting

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -6,4 +6,7 @@ public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 	public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts) {
 		super(id, date, contacts);
 	}
+	public FutureMeetingImpl(Meeting meeting) {
+		super(meeting.getId(), meeting.getDate(), meeting.getContacts());
+	}
 }

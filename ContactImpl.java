4a8f0f0121b8eb0e8fee8482233e722f9fde868c
Tanyel Bariser <tanyel19@hotmail>
2013-03-05 14:23:40 +0000

    Implemented Contact interface

---
+++ ContactImpl.java
@@ -0,0 +1,23 @@
+public class ContactImpl implements Contact {
+	private int id;
+	private String name;
+	private String notes;
+	
+	public ContactImpl(int id, String name, String notes) {
+		this.id = id;
+		this.name = name;
+		this.notes = notes;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void addNotes(String note) {
+		notes += note;
+	}
+}

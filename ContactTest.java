d8c7ecebcb748c353f3eb72c5964424dd7f16f2a
Tanyel Bariser <tanyel19@hotmail>
2013-03-05 16:55:06 +0000

    Changed Contact variable name to contact Please enter the commit message for your changes. Lines starting

--- ContactTest.java
+++ ContactTest.java
@@ -3,27 +3,27 @@ import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 public class ContactTest {
-	private Contact c;
+	private Contact contact;
 
 	@Before
 	public void buildUp() {
-		c = new ContactImpl(4, "Tanyel", "TestNote");
+		contact = new ContactImpl(4, "Tanyel", "TestNote");
 	}
 	@Test
 	public void testsGetId() {
-		assertEquals(c.getId(), 4);
+		assertEquals(contact.getId(), 4);
 	}
 	@Test
 	public void testsGetName() {
-		assertEquals(c.getName(), "Tanyel");
+		assertEquals(contact.getName(), "Tanyel");
 	}
 	@Test
 	public void testsGetNotes() {
-		assertEquals(c.getNotes(), "TestNote");
+		assertEquals(contact.getNotes(), "TestNote");
 	}
 	@Test
 	public void testsAddNotes() {
-		c.addNotes("&MoreNotes");
-		assertEquals(c.getNotes(), "TestNote&MoreNotes");
+		contact.addNotes("&MoreNotes");
+		assertEquals(contact.getNotes(), "TestNote&MoreNotes");
 	}
 }

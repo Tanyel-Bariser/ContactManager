5295a885218b0565b07f41c960fc3b9594db4469
Tanyel Bariser <tanyel19@hotmail>
2013-03-05 19:39:48 +0000

    Implemented PastMeeting interface

---
+++ PastMeetingImpl.java
@@ -0,0 +1,17 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+	private String notes = "";
+	
+	public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String note) {
+		super(id, date, contacts);
+		notes += note;
+	}
+	public PastMeetingImpl(Meeting meeting) {
+		super(meeting.getId(), meeting.getDate(), meeting.getContacts());
+	}
+	public String getNotes() {
+		return notes;
+	}
+}

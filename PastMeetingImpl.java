0dea561d38fe8961070635335459a08e569cad7a
Tanyel Bariser <tanyel19@hotmail>
2013-03-06 22:31:24 +0000

    Switched contacts and date @param

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -4,12 +4,12 @@ import java.util.Set;
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	private String notes = "";
 	
-	public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String note) {
-		super(id, date, contacts);
+	public PastMeetingImpl(int id, Set<Contact> contacts, Calendar date, String note) {
+		super(id, contacts, date);
 		notes += note + "\n";
 	}
 	public PastMeetingImpl(PastMeeting meeting) {
-		this(meeting.getId(), meeting.getDate(), meeting.getContacts(), meeting.getNotes());
+		this(meeting.getId(), meeting.getContacts(), meeting.getDate(), meeting.getNotes());
 	}
 	public String getNotes() {
 		return notes;
@@ -19,7 +19,7 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	}
 	public boolean equals(PastMeeting otherMeeting) {
 		boolean equal = false;
-		Meeting meeting = new MeetingImpl(otherMeeting.getId(), otherMeeting.getDate(), otherMeeting.getContacts());
+		Meeting meeting = new MeetingImpl(otherMeeting.getId(), otherMeeting.getContacts(), otherMeeting.getDate());
 		equal = super.equals(meeting);
 		equal = equal && getNotes().equals(otherMeeting.getNotes());
 		return equal;

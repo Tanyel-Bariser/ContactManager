790f8a23a3f4790838b0a0873ff4514206057a17
Tanyel Bariser <tanyel19@hotmail>
2013-03-07 22:59:05 +0000

    Added @After cleanUp() & removed id from parameters & removed testsEquals()

--- ContactTest.java
+++ ContactTest.java
@@ -1,19 +1,26 @@
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 public class ContactTest {
-	private ContactImpl contact, contactSame, contactNotSame;
+	private Contact contact, contactNext;
 
 	@Before
 	public void buildUp() {
-		contact = new ContactImpl(4, "Tanyel", "TestNote");
+		contact = new ContactImpl("Tanyel", "TestNote");
+		contactNext = new ContactImpl("John", "Notes");
+	}
+	@After
+	public void cleanUp() {
+		contact = null;
+		contactNext = null;
 	}
 	@Test
-	public void testsGetId() {
-		assertEquals(contact.getId(), 4);
+	public void testsGetId() {//Each time an instance of contact is created the id is incremented by 1
+		assertEquals(contact.getId(), contactNext.getId()-1);
 	}
 	@Test
 	public void testsGetName() {
@@ -27,16 +34,5 @@ public class ContactTest {
 	public void testsAddNotes() {
 		contact.addNotes("&MoreNotes");
 		assertEquals(contact.getNotes(), "TestNote\n&MoreNotes\n");
-	}
-	@Test
-	public void testsEquals() {
-		contactSame = new ContactImpl(contact);
-		contact.addNotes("");	//Replicates the additional (+ "\n") of contactSame's notes
-		assertTrue(contact.equals(contactSame));
-	}
-	@Test
-	public void testsFalseEquals() {
-		contactNotSame = new ContactImpl(5, "Tanyel", contact.getNotes());
-		assertFalse(contact.equals(contactNotSame));
 	}
 }

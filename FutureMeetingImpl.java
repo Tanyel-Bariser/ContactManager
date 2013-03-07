d3c02f99cc6cb69278592be12094be02bba3a5b0
Tanyel Bariser <tanyel19@hotmail>
2013-03-07 22:54:49 +0000

    Removed id from constructor because no longer needed

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -3,10 +3,7 @@ import java.util.Set;
 
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 	
-	public FutureMeetingImpl(int id, Set<Contact> contacts, Calendar date) {
-		super(id, contacts, date);
-	}
-	public FutureMeetingImpl(Meeting meeting) {
-		super(meeting.getId(), meeting.getContacts(), meeting.getDate());
+	public FutureMeetingImpl(Set<Contact> contacts, Calendar date) {
+		super(contacts, date);
 	}
 }

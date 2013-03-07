6364c20c8fb72e0346c3b24376d9d0790add23df
Tanyel Bariser <tanyel19@hotmail>
2013-03-07 22:55:38 +0000

    Removed id from constructor and equals() as no longer needed

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -4,24 +4,14 @@ import java.util.Set;
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	private String notes = "";
 	
-	public PastMeetingImpl(int id, Set<Contact> contacts, Calendar date, String note) {
-		super(id, contacts, date);
+	public PastMeetingImpl(Set<Contact> contacts, Calendar date, String note) {
+		super(contacts, date);
 		notes += note + "\n";
-	}
-	public PastMeetingImpl(PastMeeting meeting) {
-		this(meeting.getId(), meeting.getContacts(), meeting.getDate(), meeting.getNotes());
 	}
 	public String getNotes() {
 		return notes;
 	}
 	public void addNotes(String note) {
 		notes += note + "\n";
-	}
-	public boolean equals(PastMeeting otherMeeting) {
-		boolean equal = false;
-		Meeting meeting = new MeetingImpl(otherMeeting.getId(), otherMeeting.getContacts(), otherMeeting.getDate());
-		equal = super.equals(meeting);
-		equal = equal && getNotes().equals(otherMeeting.getNotes());
-		return equal;
 	}
 }

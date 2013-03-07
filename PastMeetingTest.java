429e9d97d6231bd6eea0f8ac02d2802c33b92536
Tanyel Bariser <tanyel19@hotmail>
2013-03-07 23:04:35 +0000

    Added @After cleanUp() & removed id from params & removed testsEquals()

--- PastMeetingTest.java
+++ PastMeetingTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import org.junit.Test;
 import org.junit.Before;
+import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -10,22 +12,32 @@ import java.util.Calendar;
 public class PastMeetingTest {
 	private Contact contact1, contact2;
 	private Set<Contact> contacts;
-	private PastMeetingImpl meeting, meetingSame;
+	private PastMeeting meeting, meetingNext;
 	private Calendar date;
 	
 	@Before
 	public void buildUp() {
-		contact1 = new ContactImpl(27, "Tanyel");
-		contact2 = new ContactImpl(21, "John");
+		contact1 = new ContactImpl("Tanyel");
+		contact2 = new ContactImpl("John");
 		contacts = new HashSet<Contact>();
 		contacts.add(contact1);
 		contacts.add(contact2);
 		date = Calendar.getInstance();
-		meeting = new PastMeetingImpl(1, contacts, date, "MeetingNotes");
+		meeting = new PastMeetingImpl(contacts, date, "MeetingNotes");
+		meetingNext = new PastMeetingImpl(contacts, date, "");
+	}
+	@After
+	public void cleanUp() {
+		contact1 = null;
+		contact2 = null;
+		contacts = null;
+		meeting = null;
+		meetingNext = null;
+		date = null;
 	}
 	@Test
-	public void testsGetId() {
-		assertEquals(1, meeting.getId());
+	public void testsGetId() {//Each time an instance of meeting is created the id is incremented by 1
+		assertEquals(meeting.getId(), meetingNext.getId()-1);
 	}
 	@Test
 	public void testsGetDate() {
@@ -38,11 +50,5 @@ public class PastMeetingTest {
 	@Test
 	public void testsGetNotes() {
 		assertEquals(meeting.getNotes(), "MeetingNotes\n");
-	}
-	@Test
-	public void testsEquals() {
-		meetingSame = new PastMeetingImpl(meeting);
-		meeting.addNotes("");	//Replicates the additional (+ "\n") of meetingSame's notes
-		assertTrue(meeting.equals(meetingSame));
 	}
 }

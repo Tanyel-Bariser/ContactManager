e91f12b178f72a80f6cf1299ff874a4aca04c658
Tanyel Bariser <tanyel19@hotmail>
2013-03-07 00:59:31 +0000

    Implemented getMeeting()

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -19,7 +19,7 @@ public class ContactManagerImpl implements ContactManager {
 			throw new IllegalArgumentException("Date of meeting to be added cannot be in the past.");
 		}
 		for (Contact contact : contacts) {
-			boolean unknownContact = !idContactsMap.containsValue(contact);	//Assigns true if does NOT contain contact
+			boolean unknownContact = !idContactsMap.containsValue(contact);//True if does NOT contain contact
 			if (unknownContact) {
 				throw new IllegalArgumentException("Set of contacts contains unknown contact(s)");
 			}
@@ -31,21 +31,21 @@ public class ContactManagerImpl implements ContactManager {
 	public PastMeeting getPastMeeting(int id) {
 		Meeting pastMeeting = idMeetingsMap.get(id);
 		if (pastMeeting == null || pastMeeting.getDate().compareTo(Calendar.getInstance()) < 0) {
-			return (PastMeeting) pastMeeting;	//Returns null if id is not mapped to meeting or returns pastMeeting if its date is in the past
-		} else {
+			return (PastMeeting) pastMeeting;	//Returns null if id is not mapped to meeting
+		} else {								// or returns pastMeeting if its date is in the past
 			throw new IllegalArgumentException("Date of meeting is in the future.");
 		}
 	}
 	public FutureMeeting getFutureMeeting(int id) {
 		Meeting futureMeeting = idMeetingsMap.get(id);
 		if (futureMeeting == null || futureMeeting.getDate().compareTo(Calendar.getInstance()) > 0) {
-			return (FutureMeeting) futureMeeting;	//Returns null if id is not mapped to meeting or returns pastMeeting if its date is in the future
-		} else {
+			return (FutureMeeting) futureMeeting;	//Returns null if id is not mapped to meeting
+		} else {									// or returns pastMeeting if its date is in the future
 			throw new IllegalArgumentException("Date of meeting is in the future.");
 		}
 	}
 	public Meeting getMeeting(int id) {
-		
+		return idMeetingsMap.get(id);
 	}
 	public List<Meeting> getFutureMeetingList(Contact contact) {
 		

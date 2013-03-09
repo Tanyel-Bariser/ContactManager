323544927fcd5ed8c65cba3c6171f094d3afe72e
Tanyel Bariser <tanyel19@hotmail>
2013-03-09 01:49:33 +0000

    Added comments

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -1,8 +1,8 @@
 import java.util.Calendar;
 import java.util.Set;
-
+//A class for future meetings
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
-	
+	//This class is only necessary for type checking and/or downcasting
 	public FutureMeetingImpl(Set<Contact> contacts, Calendar date) {
 		super(contacts, date);
 	}

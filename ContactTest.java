4983f1331c5fded1f57236648d4a124299f60b4f
Tanyel Bariser <tanyel19@hotmail>
2013-03-09 01:46:32 +0000

    Added comments

--- ContactTest.java
+++ ContactTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-
+//A class to test the methods of ContactImpl
 public class ContactTest {
 	private Contact contact, contactNext;
 

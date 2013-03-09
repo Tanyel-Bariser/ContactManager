1317ac48a1fdc84cc33fe1daab6899305c711cd9
Tanyel Bariser <tanyel19@hotmail>
2013-03-09 01:48:18 +0000

    Added comments

--- DateMeetingComparator.java
+++ DateMeetingComparator.java
@@ -1,5 +1,5 @@
 import java.util.Comparator;
-
+//A class which facilitates the ability to sort meetings by date
 public class DateMeetingComparator implements Comparator<Meeting>{
 	public int compare(Meeting meetingOne, Meeting meetingTwo) {
 		return meetingOne.getDate().compareTo(meetingTwo.getDate());

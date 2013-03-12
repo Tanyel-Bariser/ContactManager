8880ce113b1bab8fcc07d66ee54b920f4c4f0e33
Tanyel Bariser <tanyel19@hotmail>
2013-03-12 03:33:32 +0000

    Deleted import static org.junit.Assert.assertTrue;

--- PastMeetingTest.java
+++ PastMeetingTest.java
@@ -3,7 +3,6 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 import java.util.HashSet;

7ac7f9ca60b2dd3d2482241ce5ec344050fa65f7
Tanyel Bariser <tanyel19@hotmail>
2013-03-16 00:27:13 +0000

    Implements Serializable

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -1,8 +1,12 @@
 import java.util.Calendar;
 import java.util.Set;
+import java.io.Serializable;
 //A class for future meetings
-public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting, Serializable {
 	//This class is only necessary for type checking and/or downcasting
+	
+	//Constructor with no parameters as an attempt to make class serialisable
+	public FutureMeetingImpl() {}
 	public FutureMeetingImpl(Set<Contact> contacts, Calendar date) {
 		super(contacts, date);
 	}

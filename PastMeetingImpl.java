9bf5e4a9cc94edb71ba3dfb3061f596eb4056957
Tanyel Bariser <tanyel19@hotmail>
2013-03-16 00:27:54 +0000

    Implements Serializable

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -1,16 +1,20 @@
 import java.util.Calendar;
 import java.util.Set;
+import java.io.Serializable;
 //A class for past meetings including notes about what happened and what was agreed
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	private String notes = "";
 	
+	//Constructor with no parameters as an attempt to make class serialisable
+	public PastMeetingImpl() {}
 	public PastMeetingImpl(Set<Contact> contacts, Calendar date, String note) {
 		super(contacts, date);
 		notes += note + "\n";
 	}
+	//Used by the ContactManager.addMeetingNotes() method to convert to PastMeeting type and keep same id
 	public PastMeetingImpl(int id, Set<Contact> contacts, Calendar date, String note) {
-		super(id, contacts, date);//Only used by ContactManager.addMeetingNotes() to convert from
-		notes += note + "\n";//future to past meeting otherwise id is give by idStatic in MeetingImpl
+		super(id, contacts, date);
+		notes += note + "\n";
 	}
 	public String getNotes() {
 		return notes;

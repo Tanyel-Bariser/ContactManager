5afe4fdb33da419d3134f4458517ac299bf47153
Tanyel Bariser <tanyel19@hotmail>
2013-03-16 00:26:44 +0000

    Implements Serializable

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,12 +1,16 @@
+import java.io.Serializable;
 //A contact is a person we are making business with or may do in the future.
-public class ContactImpl implements Contact {
+public class ContactImpl implements Contact, Serializable {
 	private static int idStatic = 0;
 	private int id;
 	private String name;
 	private String notes;
 	
+	//Constructor with no parameters as an attempt to make class serialisable
+	public ContactImpl() {}
 	public ContactImpl(String name, String newNote) {
-		id = idStatic++;//Prevents more than one contact having the same id
+		//Prevents more than one contact having the same id
+		id = idStatic++;
 		this.name = name;
 		notes = newNote + "\n";
 	}

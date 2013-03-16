2506ce290be0d2d479da3799a3afe5fa064f2f4d
Tanyel Bariser <tanyel19@hotmail>
2013-03-16 00:27:36 +0000

    Implements Serializable

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -1,20 +1,24 @@
 import java.util.Calendar;
 import java.util.Set;
+import java.io.Serializable;
 //Meetings have unique IDs, scheduled date and a list of participating contacts
-public abstract class MeetingImpl implements Meeting {
+public abstract class MeetingImpl implements Meeting, Serializable {
 	private static int idStatic = 0;//Prevents more than one meeting having the same id
 	private int id;
 	private Calendar date;
 	private Set<Contact> contacts;
 	
+	//Constructor with no parameters as an attempt to make class serialisable
+	public MeetingImpl() {}
 	public MeetingImpl(Set<Contact> contacts, Calendar date) {
 		id = idStatic++;
 		this.date = date;
 		this.contacts = contacts;
 	}
-	public MeetingImpl(int id, Set<Contact> contacts, Calendar date) {//Only used for past meetings
-		this.id = id;//Only used by ContactManager.addMeetingNotes() to convert from future
-		this.date = date;//to past meeting otherwise id is give by idStatic in MeetingImpl
+	//Only for PastMeetingImpl. Used by the ContactManager.addMeetingNotes() method to convert to PastMeeting type and keep same id
+	public MeetingImpl(int id, Set<Contact> contacts, Calendar date) {
+		this.id = id;
+		this.date = date;
 		this.contacts = contacts;
 	}
 	public int getId() {

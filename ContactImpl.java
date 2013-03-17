342f0efdb4b778dc401ddb4fe989c8801d784d27
Tanyel Bariser <tanyel19@hotmail>
2013-03-17 01:02:32 +0000

    Javadoc and replaced tab indentations with 4 spaces - Keith's suggestion

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,32 +1,71 @@
 import java.io.Serializable;
-//A contact is a person we are making business with or may do in the future.
+/**
+* A contact is a person we are making business with or may do in the future.
+*
+* Contacts have an ID (unique), a name (probably unique, but maybe
+* not), and notes that the user may want to save about them.
+*/
 public class ContactImpl implements Contact, Serializable {
-	private static int idStatic = 0;
-	private int id;
-	private String name;
-	private String notes;
+    private static int idStatic = 0;
+    private int id;
+    private String name;
+    private String notes;
 	
-	//Constructor with no parameters as an attempt to make class serialisable
-	public ContactImpl() {}
-	public ContactImpl(String name, String newNote) {
-		//Prevents more than one contact having the same id
-		id = idStatic++;
-		this.name = name;
-		notes = newNote + "\n";
-	}
-	public ContactImpl(String name) {
-		this(name, "");
-	}
-	public int getId() {
-		return id;
-	}
-	public String getName() {
-		return name;
-	}
-	public String getNotes() {
-		return notes;
-	}
-	public void addNotes(String newNote) {
-		notes += newNote + "\n";
-	}		
+    //Constructor with no parameters as an attempt to make class serialisable
+    public ContactImpl() {}
+    /**
+    * Constructor for contact
+    *
+    * @param name of contact
+    * @param newNote for contact
+    */
+    public ContactImpl(String name, String newNote) {
+        //Prevents more than one contact having the same id
+        id = idStatic++;
+        this.name = name;
+        notes = newNote + "\n";
+    }
+    /**
+    * Second constructor that only takes name
+    *
+    * @param name of contact
+    */
+    public ContactImpl(String name) {
+        this(name, "");
+    }
+    /**
+    * Returns the ID of the contact.
+    *
+    * @return the ID of the contact.
+    */
+    public int getId() {
+        return id;
+    }
+    /**
+    * Returns the name of the contact.
+    *
+    * @return the name of the contact.
+    */
+    public String getName() {
+        return name;
+    }
+    /**
+    * Returns our notes about the contact, if any.
+    *
+    * If we have not written anything about the contact, the empty
+    * string is returned.
+    *
+    * @return a string with notes about the contact, maybe empty.
+    */
+    public String getNotes() {
+        return notes;
+    }
+    /**
+    * Add notes about the contact.
+    *
+    * @param note the notes to be added
+    */
+    public void addNotes(String newNote) {
+        notes += newNote + "\n";
+    }		
 }

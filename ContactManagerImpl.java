18c36eb88f299bef29413426163a96e1ce9fbfa0
Tanyel Bariser <tanyel19@hotmail>
2013-03-17 02:17:59 +0000

    Small change to Javadoc

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
 /**
 * A class to manage your contacts and meetings.
 */

53f7e829892eef09aab624b72ad67ac2a9901fb5
Tanyel Bariser <tanyel19@hotmail>
2013-03-17 01:39:29 +0000

    Added Javadoc and replaced tab indents with 4 spaces - Keith's suggestion

--- DateMeetingComparator.java
+++ DateMeetingComparator.java
@@ -1,8 +1,15 @@
 import java.util.Comparator;
-//A class which facilitates the ability to sort meetings by date
+/**
+* A class which facilitates the ability to sort meetings by date
+*/
 public class DateMeetingComparator implements Comparator<Meeting>{
-	public int compare(Meeting meetingOne, Meeting meetingTwo) {
-		return meetingOne.getDate().compareTo(meetingTwo.getDate());
-	}
-
+    /**
+    * Sorts meetings dates in chronological order
+    *
+    * @param meetingOne first meeting to compare to second meeting
+    * @param meetingTwo second meeting to compare to first meeting
+    */
+    public int compare(Meeting meetingOne, Meeting meetingTwo) {
+        return meetingOne.getDate().compareTo(meetingTwo.getDate());
+    }
 }
